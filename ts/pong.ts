e730a27b2ff6bd37f1dcb4eb74293e1cd9a047a6
Dan <k.daniel.adams@gmail.com>
2020-01-16 16:13:36 -0500

    revise Pong constructor

--- ts/pong.ts
+++ ts/pong.ts
@@ -11,14 +11,17 @@ export class Pong {
     constructor(canvas: HTMLCanvasElement) {
         let ball_x = 0,
             ball_y = 0,
-            coord_y = (canvas.width / 2) - 50,
-            coord_x_1 = 15,
-            coord_x_2 = canvas.width - 35;
+            margin = 15,
+            player_width = 20,
+            player_height = 100,
+            player_y = (canvas.height / 2) - (player_height / 2),
+            player_1_x = margin,
+            player_2_x = canvas.width - player_width - margin;
 
         this.canvas = canvas;
         this.ctx = this.canvas.getContext('2d');
-        this.player_1 = new Player(coord_x_1, coord_y);
-        this.player_2 = new Player(coord_x_2, coord_y);
+        this.player_1 = new Player(player_1_x, player_y);
+        this.player_2 = new Player(player_2_x, player_y);
 
         ball_x = this.canvas.width / 2;
         ball_y = this.canvas.height / 2;
